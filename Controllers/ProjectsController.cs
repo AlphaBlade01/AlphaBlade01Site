ab83ed9530c08792b468691db0a449b535ec582b
AlphaBlade01 <ayaanuppal99@gmail.com>
2023-06-30 23:38:53 +0100

    feature: initial commit of new projects page

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlphaBlade01.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
