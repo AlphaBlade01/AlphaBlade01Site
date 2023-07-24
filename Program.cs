6e18e80b1119bde5cc6a193834eee2466f92061d
AlphaBlade01 <ayaanuppal99@gmail.com>
2023-07-24 19:02:26 +0100

    change: changing from sqlite to postgresql & adding cookie authentication

--- Program.cs
+++ Program.cs
@@ -1,16 +1,23 @@
 using AlphaBlade01.Logic.Contexts;
-using Microsoft.EntityFrameworkCore;
+using AlphaBlade01.Logic.Models.DTOs;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ProjectsContext>(context =>
-{
-    context.UseSqlite(@"Data Source = Data/projects.db");
-});
+builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie();
+builder.Services.AddAuthorization();
+builder.Services.AddIdentity<UserDTO, RoleDTO>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -25,6 +32,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
