54fc370cf1ab83f6423a8e10c910f4b5b486ef7b
AlphaBlade01 <ayaanuppal99@gmail.com>
2023-07-24 19:03:28 +0100

    change: uninstalled sqlite packages and installed postgres packages + identity packages

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using AlphaBlade01.Logic.Contexts;
+using AlphaBlade01.Logic.Models.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlphaBlade01.Controllers
+{
+	public class AccountController : Controller
+	{
+		private readonly ApplicationDbContext _context;
+		private readonly UserManager<UserDTO> _userManager;
+		private readonly SignInManager<UserDTO> _signInManager;
+		public readonly IPasswordHasher<UserDTO> _passwordHasher;
+
+		public AccountController(UserManager<UserDTO> userManager, SignInManager<UserDTO> signInManager, IPasswordHasher<UserDTO> passwordHasher, ApplicationDbContext context)
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_passwordHasher = passwordHasher;
+			_context = context;
+		}
+
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(InputUserModel userDto)
+		{
+			UserDTO user = new UserDTO();
+			string hashedPassword = _passwordHasher.HashPassword(user, userDto.Password);
+
+			user.UserName = userDto.UserName;
+			user.PasswordHash = hashedPassword;
+			user.SecurityStamp = Guid.NewGuid().ToString();
+
+			_context.Users.Add(user);
+			_context.SaveChanges();
+
+			await _userManager.AddToRoleAsync(user, "GUEST");
+			await _signInManager.SignInAsync(user, isPersistent: false);
+
+			return Redirect("/");
+		}
+
+		public IActionResult Login()
+		{
+			if (HttpContext.User.Identity is not null) return Redirect("/");
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Login(InputUserModel userDto)
+		{
+			if (HttpContext.User.Identity is not null) return Redirect("/");
+
+			UserDTO? user = await _userManager.FindByNameAsync(userDto.UserName);
+			if (user == null | !await _userManager.CheckPasswordAsync(user, userDto.Password)) return RedirectToAction("Login");
+
+			await _signInManager.SignInAsync(user, isPersistent: false);
+			return Redirect("/");
+		}
+	}
+}
