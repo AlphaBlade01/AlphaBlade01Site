74d6f291a699bc448d7d8badc1fa05981fe5544e
AlphaBlade01 <ayaanuppal99@gmail.com>
2023-07-24 18:59:27 +0100

    change: switching from sqlite to postgresql

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -1,47 +1,47 @@
 ﻿using AlphaBlade01.Logic.Contexts;
-using AlphaBlade01.Logic.Models;
+using AlphaBlade01.Logic.Models.DTOs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Immutable;
 
 namespace AlphaBlade01.Controllers
 {
-    public class ProjectsController : Controller
+	public class ProjectsController : Controller
     {
-        private IHostEnvironment _hostingEnvironment;
+        private readonly IHostEnvironment _hostingEnvironment;
+        private readonly ApplicationDbContext _context;
 
-        public ProjectsController(IHostEnvironment environment) 
+        public ProjectsController(IHostEnvironment environment, ApplicationDbContext context) 
         { 
             _hostingEnvironment = environment;
+            _context = context;
         }
 
 
         public IActionResult Index()
         {
-            using ProjectsContext db = new();
-            ImmutableArray<ProjectModel> projects = db.Set<ProjectModel>().ToImmutableArray();
-
+            ImmutableArray<ProjectDTO> projects = _context.Set<ProjectDTO>().ToImmutableArray();
             return View("Index", projects);
         }
 
+        [Authorize(Roles = "Admin")]
         public IActionResult Add()
         {
             return View();
         }
 
+        [Authorize(Roles = "Admin")]
         [HttpPost]
-        public async Task<IActionResult> Add(FormProjectsModel httpModel)
+        public async Task<IActionResult> Add(InputProjectModel httpModel)
         {
             if (!ModelState.IsValid) return View();
-            ProjectModel model = httpModel.CastToModel();
+            ProjectDTO model = httpModel.CastToModel();
 
             if (model.DateUploaded == default)
-                model.DateUploaded = DateTime.Now;
+                model.DateUploaded = DateTime.Now.ToUniversalTime();
 
-            using (var db = new ProjectsContext())
-            {
-                db.Add(model);
-                db.SaveChanges();
-            }
+            _context.Add(model);
+            _context.SaveChanges();
 
             if (httpModel.Thumbnail is not null)
             {
