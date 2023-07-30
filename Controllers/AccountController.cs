1754e5c5267737405843f4569781bc3bfbfd0c05
AlphaBlade01 <ayaanuppal99@gmail.com>
2023-07-30 22:22:30 +0100

    feature: added account overview & server-sided validation

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using AlphaBlade01.Logic.Models.DTOs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Principal;
 
 namespace AlphaBlade01.Controllers
 {
@@ -20,7 +21,36 @@ namespace AlphaBlade01.Controllers
 			_context = context;
 		}
 
+		public async Task<IActionResult> Index()
+		{
+			string? userName = User.Identity?.Name;
+			if (userName is null) return View();
+
+			UserDTO? user = await _userManager.FindByNameAsync(userName);
+			if (user == null) return View();
+
+			string role = (await _userManager.GetRolesAsync(user))[0];
+
+			ViewBag.UserName = userName;
+			ViewBag.Role = role;
+
+			return View();
+		}
+
 		public IActionResult Register()
+		{
+			if ((bool)HttpContext.User.Identity?.IsAuthenticated) return Redirect("/");
+			return View();
+		}
+
+		public IActionResult Login()
+		{
+			if ((bool)HttpContext.User.Identity?.IsAuthenticated) return Redirect("/");
+			ViewData["Error"] = TempData["Error"];
+			return View();
+		}
+
+		public IActionResult AccessDenied()
 		{
 			return View();
 		}
@@ -28,6 +58,9 @@ namespace AlphaBlade01.Controllers
 		[HttpPost]
 		public async Task<IActionResult> Register(InputUserModel userDto)
 		{
+			if (!ModelState.IsValid) return RedirectToAction(nameof(Register));
+			if ((bool)HttpContext.User.Identity?.IsAuthenticated) return Redirect("/");
+
 			UserDTO user = new UserDTO();
 			string hashedPassword = _passwordHasher.HashPassword(user, userDto.Password);
 
@@ -44,22 +77,29 @@ namespace AlphaBlade01.Controllers
 			return Redirect("/");
 		}
 
-		public IActionResult Login()
-		{
-			if (HttpContext.User.Identity is not null) return Redirect("/");
-			return View();
-		}
-
 		[HttpPost]
 		public async Task<IActionResult> Login(InputUserModel userDto)
 		{
-			if (HttpContext.User.Identity is not null) return Redirect("/");
+			if (!ModelState.IsValid) return RedirectToAction(nameof(Login));
+			if ((bool)HttpContext.User.Identity?.IsAuthenticated) return Redirect("/");
 
 			UserDTO? user = await _userManager.FindByNameAsync(userDto.UserName);
-			if (user == null | !await _userManager.CheckPasswordAsync(user, userDto.Password)) return RedirectToAction("Login");
+			if (user == null | !await _userManager.CheckPasswordAsync(user, userDto.Password))
+			{
+				TempData["Error"] = "Incorrect username or password";
+				return RedirectToAction("Login");
+			}
 
 			await _signInManager.SignInAsync(user, isPersistent: false);
 			return Redirect("/");
 		}
+
+		[HttpPost]
+		public async Task<IActionResult> Logout()
+		{
+			await _signInManager.SignOutAsync();
+			return Redirect("/");
+		}
+
 	}
 }
