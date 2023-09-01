1ceed62501fb4ff819a22abff568b4c2429486f4
AlphaBlade01 <ayaanuppal99@gmail.com>
2023-09-01 14:36:33 +0100

    small: switched from service to systemctl

--- publish.bat
+++ publish.bat
@@ -6,5 +6,5 @@ ECHO Compiled
 scp -r  ./bin/Release/net7.0/publish/* admin@raspberrypi:/var/www/alphablade01/
 ECHO Transferred
 
-ssh admin@raspberrypi "sudo service alphablade01 restart"
+ssh admin@raspberrypi "sudo systemctl restart alphablade01"
 ECHO Restarted server
