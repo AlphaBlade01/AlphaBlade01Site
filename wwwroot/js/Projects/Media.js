56501120dbcad01fac30db67f2258986a9d42bfd
AlphaBlade01 <ayaanuppal99@gmail.com>
2023-09-05 12:01:30 +0100

    feature: added preview for media inputs

---
+++ wwwroot/js/Projects/Media.js
@@ -0,0 +1,30 @@
+﻿const thumbnail_input = document.getElementById("Thumbnail");
+const previews_input = document.getElementById("Previews");
+const thumbnail_preview = document.getElementById("thumbnail-preview");
+const previews_container = document.getElementById("previews-container");
+const meta_img = previews_container.getElementsByClassName("meta")[0];
+
+thumbnail_input.onchange = (event) => {
+    const reader = new FileReader();
+    reader.onload = () => thumbnail_preview.src = reader.result;
+    reader.readAsDataURL(event.target.files[0]);
+};
+
+previews_input.onchange = (event) => {
+    while (meta_img.nextSibling) {
+        meta_img.nextSibling.remove();
+    }
+
+    setTimeout(() => {
+        for (const file of event.target.files) {
+            const reader = new FileReader();
+            const img_element = meta_img.cloneNode();
+
+            img_element.className = "preview"
+            previews_container.appendChild(img_element);
+
+            reader.onload = () => img_element.src = reader.result;
+            reader.readAsDataURL(file);
+        }
+    }, 2000);
+}
